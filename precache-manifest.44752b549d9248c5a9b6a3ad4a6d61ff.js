823de3608b4336c59b003f33966ea472e520ec6a
Charles Diep <charlesdiep91@gmail.com>
2018-11-19 16:18:14 +0100

    Updates

--- precache-manifest.44752b549d9248c5a9b6a3ad4a6d61ff.js
+++ precache-manifest.44752b549d9248c5a9b6a3ad4a6d61ff.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/Movie-lib/static/js/runtime~main.dbaf860a.js"
   },
   {
-    "revision": "afb58f5e9138bfecb459",
-    "url": "/Movie-lib/static/js/main.afb58f5e.chunk.js"
+    "revision": "534cec328877053e0929",
+    "url": "/Movie-lib/static/js/main.534cec32.chunk.js"
   },
   {
     "revision": "e3e7dc295963c73c465e",
     "url": "/Movie-lib/static/js/1.e3e7dc29.chunk.js"
   },
   {
-    "revision": "afb58f5e9138bfecb459",
-    "url": "/Movie-lib/static/css/main.676248cf.chunk.css"
+    "revision": "534cec328877053e0929",
+    "url": "/Movie-lib/static/css/main.344df7ed.chunk.css"
   },
   {
     "revision": "e3e7dc295963c73c465e",
     "url": "/Movie-lib/static/css/1.60f652ab.chunk.css"
   },
   {
-    "revision": "b0d56acc5d7b0cb58953cdb52d22dd25",
+    "revision": "66444e5f313fe233a2f9c90a711bcbe5",
     "url": "/Movie-lib/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Movie-lib/precache-manifest.dfb60df3281d093b1fe023c3abeaa85c.js"
+  "/Movie-lib/precache-manifest.44752b549d9248c5a9b6a3ad4a6d61ff.js"
 );
 
 workbox.clientsClaim();
